5e7213fae7b7caf692b8bd8b94f45ecbe040dbc6
Harry Karvonen <harry.karvonen@gmail.com>
2012-07-15 20:19:02 +0300

    Added simple ruler. Click image to see frequency/time and click ruler to hide them.

--- PulseAudioSpectrogram.py
+++ PulseAudioSpectrogram.py
@@ -23,7 +23,7 @@ import spectrogram
 
 import math
 import cairo
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, Gdk, GObject
 
 class SpectrogramWidget(Gtk.DrawingArea):
   def __init__(self):
@@ -33,10 +33,15 @@ class SpectrogramWidget(Gtk.DrawingArea):
     self.red_dB_offset = 0
     self.red_dB_max = 18
     #self.bufLabel = Gtk.Label()
+    self.press_x = -1
+    self.press_y = -1
+
+    self.connect("button-press-event", self.on_mouse_press_event)
+    self.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
 
     spectrogram.connect()
 
-    self.set_size_request(1024, 512)
+    self.set_size_request(1024 + 20, 512 + 20)
 
     self.connect("draw", self.on_draw)
 
@@ -54,6 +59,11 @@ class SpectrogramWidget(Gtk.DrawingArea):
   def stop(self):
     self.running = False
     pass # def stop
+
+  def on_mouse_press_event(self, widget, event):
+    self.press_x, self.press_y = event.get_coords()
+    self.get_window().invalidate_rect(self.get_allocation(), True)
+    pass # def on_mouse_press_event
 
   def on_timeout(self):
     my_cr = cairo.Context(self.surface)
@@ -83,6 +93,8 @@ class SpectrogramWidget(Gtk.DrawingArea):
     cr.rectangle(0, 0, w, h)
     cr.fill()
 
+    self.draw_ruler(cr)
+
     pass # def on_draw
 
   def draw_fft(self, cr, fft):
@@ -103,6 +115,67 @@ class SpectrogramWidget(Gtk.DrawingArea):
       pass # for i in range(0, len(fft))
 
     pass # def draw_fft
+
+  def draw_ruler(self, cr):
+
+    cr.set_source_rgb(1, 1, 1)
+    cr.rectangle(1024, 0, 20, 532)
+    cr.fill()
+
+    cr.rectangle(0, 512, 1044, 20)
+    cr.fill()
+
+    cr.set_source_rgb(0, 0, 0)
+    cr.move_to(1024, 0)
+    cr.line_to(1024, 512)
+    cr.line_to(0, 512)
+    cr.stroke()
+
+    cr.move_to(0, 531)
+    cr.line_to(1043, 531)
+    cr.line_to(1043, 0)
+    cr.stroke()
+
+    if -1 < self.press_y < 512 and -1 < self.press_x < 1024:
+      fix_y = -2
+      fix_x = -2
+
+      Hz_str = "%d Hz" % int(float(512 - int(self.press_y)) * 22050.0 / 512.0)
+      s_str = "%.2f s" % ((1023 - self.press_x) * 0.025)
+
+      if self.press_y > 256:
+        fix_y = cr.text_extents(Hz_str)[2] + 4
+
+      if self.press_x > 512:
+        fix_x = cr.text_extents(s_str)[2] + 4
+
+      cr.move_to(1030, self.press_y - fix_y)
+      cr.rotate(3.14 / 2)
+      cr.show_text(Hz_str)
+
+      cr.rotate(-3.14 / 2)
+      cr.move_to(1024, self.press_y)
+      cr.line_to(1044, self.press_y)
+      cr.stroke()
+
+      cr.move_to(self.press_x - fix_x, 526)
+      cr.show_text(s_str)
+
+      cr.move_to(self.press_x, 512)
+      cr.line_to(self.press_x, 532)
+      cr.stroke()
+
+      cr.set_source_rgba(1, 1, 1, 0.2)
+      cr.move_to(self.press_x, 0)
+      cr.line_to(self.press_x, 511)
+      cr.move_to(0, self.press_y)
+      cr.line_to(self.press_x - 1, self.press_y)
+      cr.move_to(self.press_x + 1, self.press_y)
+      cr.line_to(1023, self.press_y)
+      cr.stroke()
+
+      pass # for i in range(0, 512)
+    pass
 
   pass # class SpectrogramWidget
 
